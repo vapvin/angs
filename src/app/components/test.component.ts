f7fb72b9b65ffae091b50815482418f091e0dc2e
vapvin <tpehdj12@daum.net>
2020-04-07 18:12:11 +0900

    Test Components

---
+++ src/app/components/test.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-test',
+    template: `<p>Test Component</p>`,
+    styles: [`
+        p {
+            color: steelblue;
+        }
+    `]
+})
+export class Test {
+
+}
