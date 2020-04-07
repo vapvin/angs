e5ae2652212f30491215f8daa5ed42f73adf888c
vapvin <tpehdj12@daum.net>
2020-04-07 18:24:06 +0900

    Test Simple

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Test } from './components/test.component';
+import { Test2 } from './components/test/test2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Test,
+    Test2
   ],
   imports: [
     BrowserModule
